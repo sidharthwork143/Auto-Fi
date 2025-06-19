13e5f36bf9ba3cebe2c79175fea254d25f9b4f81
Dengerous <99312935+dengerous53@users.noreply.github.com>
2025-06-19 21:04:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1102,7 +1102,7 @@ async def auto_filter(client, msg, s, spoll=False):
         ]   
     if offset != "":
         if settings['shortlink'] and not await is_premium(message.from_user.id, client):
-        btn.append(
+        btn.append(0,
             [InlineKeyboardButton(text=f"1/{math.ceil(int(total_results) / MAX_BTN)}", callback_data="buttons"),
              InlineKeyboardButton(text="ɴᴇxᴛ »", callback_data=f"next_{req}_{key}_{offset}")]
         )
